ea6703353abd65157d9cf8e105cab6c54252f93b
garyAdam <43093216+garyAdam@users.noreply.github.com>
2019-08-12 21:36:03 +0200

    upload endpoint wip

--- VidekVideoAPI/Controllers/VideosController.cs
+++ VidekVideoAPI/Controllers/VideosController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -42,13 +44,40 @@ namespace VidekVideoAPI.Controllers
         }
 
         // POST: api/Videos
-        [HttpPost]
-        public async Task<ActionResult<Video>> PostVideo(Video video)
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<ActionResult> PostVideo(Video video)
         {
-            _context.Video.Add(video);
-            await _context.SaveChangesAsync();
+          
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
 
-            return CreatedAtAction("GetVideo", new { id = video.Id }, video);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    _context.Video.Add(video);
+                    await _context.SaveChangesAsync();
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+            
         }
 
         // DELETE: api/Videos/5
